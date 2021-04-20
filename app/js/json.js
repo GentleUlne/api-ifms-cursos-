0a777d9aad91747a6da190e6888057e754927fd2
GentleUlne <augustonavirai@gmail.com>
2021-04-19 21:33:51 -0300

    ~"codificação terceira parte"

--- app/js/json.js
+++ app/js/json.js
@@ -1,110 +1,91 @@
 
     const apiData     =  document.querySelector(".api-data")
-   const spinner = document.querySelector('.spinner-grow')
+   const spinner = document.querySelector('.spinner-border')
    const tipoCursoFilter = document.querySelector('.tipoCurso-filter')
+
+  spinner.style.display="none"
 
 
     async function listarCursos(){
-   const url   = "http://localhost:3000/Curso"
+        const url   = "http://localhost:3000/Curso"
+        spinner.style.display="block"
+        const response   = await axios.get(url)
+        spinner.style.display="none"
 
-   const response   = await axios.get(url)
-  
+        const CursoList = Array.from(response.data)
+           CursoList.forEach(function(Curso)
 
-  const CursoList = Array.from(response.data)
-  CursoList.forEach(function(Curso)
-
-  {
+                       {
       
-    apiData.innerHTML += 
-   ` 
-   <div class ="card m-2"  style="width:522px height:522px"  > 
-   <section class="card-body"><h2>${Curso.nome_curso}</h2>
+                          apiData.innerHTML += 
+                           ` 
+                             <div class ="card m-2"  style="width:522px height:522px"  > 
+                             <section class="card-body"><h2>${Curso.nome_curso}</h2>
    
-   <p>nível de ensino:
-   ${Curso.nivel_de_ensino}
-    </p>
+                             <p>nível de ensino:${Curso.nivel_de_ensino} </p>
+                             
+                             <p> Duração:  ${Curso.duração} </p> 
+                           
+                                              
+                              <p>Município: ${Curso.município}</p>
   
-   <p> Duração:
-   ${Curso.duração}
-    </p>
-   <p>Município:
-   ${Curso.município} 
-   </p> 
-   </section>
+                   </section>       </div>      </br>   `        }); 
    
-
    
-  </div>
-   </br>
-    
-   `
    
-  });
-   
-
-
 
 
 
    async function search(query){
-    const url =  `http://localhost:3000/Curso?q=${query}`
- 
- const response   = await axios.get(url)
-  const CursoList  = response.data
-
-  
-  apiData.innerHTML=""
-  CursoList.forEach(function(Curso) {
+           const url =  `http://localhost:3000/Curso?q=${query}`
+  //         const response   = await axios.get(url)
+        
+           apiData.innerHTML=""
+           spinner.style.display="block"
+       
+           const response   = await axios.get(url)
+           const CursoList  = response.data
+          
+           spinner.style.display="none"
       
-    apiData.innerHTML += 
-   ` 
-   <div class ="card m-2"  style="width:522px height:522px"  > 
-   <section class="card-body"><h2>${Curso.nome_curso}</h2>
+             CursoList.forEach(function(Curso) {
+      
+          apiData.innerHTML +=` <div class ="card m-2"  style="width:522px height:522px"  > 
+                                 <section class="card-body"><h2>${Curso.nome_curso}</h2>
    
-   <p>nível de ensino:
-   ${Curso.nivel_de_ensino}
-    </p>
+                                  <p>nível de ensino:${Curso.nivel_de_ensino} </p>
+                                  <p> Duração:  ${Curso.duração} </p>
+                                  <p>Município:  ${Curso.município}  </p> 
+   
+                                  </section></div></br>`});  }
   
-   <p> Duração:
-   ${Curso.duração}
-    </p>
-   <p>Município:
-   ${Curso.município} 
-   </p> 
-   </section>
    
-
-   
-  </div>
-   </br>
     
-   `
    
-  });
    
-   }
-
-
-
-
-
-
-
-
   
+   
+ 
+
+
+    tipoCursoFilter.addEventListener('change',function(){
+      apiData.innerHTML=""
+      spinner.style.display="block"
+      spinner.style.display="none"
+      
+
+                     search(tipoCursoFilter.value)
+                                                       })
 
 
 
-   tipoCursoFilter.addEventListener('change',function(){
-  search(tipoCursoFilter.value)
-})
-
-
-
-   const  brn_buscar = document.querySelector('.btn_Buscar')
+   const  btn_buscar = document.querySelector('.btn_Buscar')
     const input_search = document.querySelector('input[type=search]')
-   brn_buscar.addEventListener('click',function(){
+
+
+   btn_buscar.addEventListener('click',function(){
    search(input_search.value)
+   
    console.log(input_search.value)
    console.log('clciado')
    })
@@ -113,7 +94,7 @@
     }
   
     
-    listarCursos()
+   
     
    async function ListarTipoCurso(){
     const url =`http://localhost:3000/tipoCurso`
@@ -124,4 +105,19 @@
     })
 
 }
+listarCursos()
 ListarTipoCurso()
+
+
+
+
+
+
+
+
+
+
+
+ //----------------------------------
+   //----------------------------------
+
